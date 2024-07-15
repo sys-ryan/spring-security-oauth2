6c18f1cc6139e9c2fe3b2488e4ce7a6f5467a157
Seongjin Kim <sjkim.a@imagoworks.ai>
2024-07-15 17:30:59 +0900

    [8-2] OAuth2AuthorizedClient

---
+++ src/main/java/com/example/springsecurityoauth2/ClientController.java
@@ -0,0 +1,62 @@
+package com.example.springsecurityoauth2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAUth2AuthorizedClientRepository;
+
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    @GetMapping("/client")
+    public String client(HttpServletRequest request, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String clientRegistrationId = "keycloak";
+
+
+        OAuth2AuthorizedClient authorizedClient = oAUth2AuthorizedClientRepository.loadAuthorizedClient(clientRegistrationId, authentication, request);
+
+        OAuth2AuthorizedClient authorizedClient2 = oAuth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, authentication.getName());
+
+
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+
+        OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(new OAuth2UserRequest(authorizedClient.getClientRegistration(), accessToken));
+
+
+        // 인증 객체 생성
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = new OAuth2AuthenticationToken(oAuth2User, Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")), clientRegistrationId);
+        SecurityContextHolder.getContext().setAuthentication(oAuth2AuthenticationToken);
+
+        model.addAttribute("accessToken", accessToken.getTokenValue());
+        model.addAttribute("refreshToken", authorizedClient.getRefreshToken());
+        model.addAttribute("principalName", oAuth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+
+        return "client";
+    }
+}
--- src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,33 +1,31 @@
 package com.example.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
+//    @Autowired
+//    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
-                        .requestMatchers("/home", "/login").permitAll()
+
+                        .requestMatchers("/home", "/client").permitAll()
+
                         .anyRequest().authenticated()
-        );
-        http.oauth2Login(authLogin -> authLogin.authorizationEndpoint(authEndpoint -> authEndpoint.authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())));
+        )
+                .oauth2Client(Customizer.withDefaults())
+
+                ;
+
         http.logout(logout -> logout.logoutSuccessUrl("/home"));
-
         return http.build();
-    }
-
-    private OAuth2AuthorizationRequestResolver customOAuth2AuthorizationRequestResolver() {
-        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 }
